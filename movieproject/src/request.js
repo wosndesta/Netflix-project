0ddc3afb812cc9e2bcb898268a5bd83e9375e473
wosndesta <87792902+wosndesta@users.noreply.github.com>
2021-12-20 21:55:03 -0500

    tried to find url

--- movieproject/src/request.js
+++ movieproject/src/request.js
@@ -1,13 +1,13 @@
-const Api_Key = "1d7d1bcc64374e0d77d619e34bdc415e";
+const API_KEY = "1d7d1bcc64374e0d77d619e34bdc415e";
  
 const request = {
-	fetchTrending: `/trending/all/week?Api_Key=${Api_Key}&language=en-US`,
-	fetchPopularOnNetflix: `/discover/tv?Api_Key=${Api_Key}&with_networks=213`,
-	fetchTopRatedMovies: `/movie/top_rated?Api_Key=${Api_Key}&language=en-US`,
-	fetchRealStories: `/discover/movie?Api_Key=${Api_Key}&with_genres=28`,
-	fetchComedyMovies: `/discover/movie?Api_Key=${Api_Key}&with_genres=35`,
-	fetchHorrorMovies: `/discover/movie?Api_Key=${Api_Key}&with_genres=27`,
-	fetchRomanceMovies: `/discover/movie?Api_Key=${Api_Key}&with_genres=10749`,
-	fetchDocumentaries: `/discover/movie?Api_Key=${Api_Key}&with_genres=99`,
+	fetchTrending: `/trending/all/week?API_KEY=${API_KEY}&language=en-US`,
+	fetchPopularOnNetflix: `/discover/tv?API_KEY=${API_KEY}&with_networks=213`,
+	fetchTopRatedMovies: `/movie/top_rated?API_KEY=${API_KEY}&language=en-US`,
+	fetchRealStories: `/discover/movie?API_KEY=${API_KEY}&with_genres=28`,
+	fetchComedyMovies: `/discover/movie?API_KEY=${API_KEY}&with_genres=35`,
+	fetchHorrorMovies: `/discover/movie?API_KEY=${API_KEY}&with_genres=27`,
+	fetchRomanceMovies: `/discover/movie?API_KEY=${API_KEY}&with_genres=10749`,
+	fetchDocumentaries: `/discover/movie?API_KEY=${API_KEY}&with_genres=99`,
 };
 export default request;
